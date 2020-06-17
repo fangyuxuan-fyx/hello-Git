a025eae29a3eb1bf01bb1b7c46c8c22fa9eb0834
fangyuxuan-fyx <2635605396@qq.com>
2020-06-17 12:35:01 +0800

    again

--- MySeven/app/src/main/java/com/example/myseven/SecondActivity.java
+++ MySeven/app/src/main/java/com/example/myseven/SecondActivity.java
@@ -9,11 +9,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SecondActivity extends AppCompatActivity {
     private static final String TAG = "SecondActivity";
+
     private String newId;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,25 +61,37 @@ public class SecondActivity extends AppCompatActivity {
         });
 
         Button button_query = findViewById(R.id.query);
+
         button_query.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                List<InfoBean> contactList = new ArrayList<InfoBean>();
                 Uri uri = Uri.parse("content://com.example.myseven.provider/Contacts");
                 Cursor cursor = getContentResolver().query(uri, null, null, null,null);
                 if(cursor != null){
                     while (cursor.moveToNext()){
-                        String id = cursor.getString(cursor.getColumnIndex("id"));
-                        String name = cursor.getString(cursor.getColumnIndex("name"));
-                        String phone = cursor.getString(cursor.getColumnIndex("phone"));
-                        String sex = cursor.getString(cursor.getColumnIndex("sex"));
 
-                        Log.d(TAG, "id："+id);
+                        InfoBean info=new InfoBean();
+                        info . id = cursor.getInt(cursor.getColumnIndex("id"));
+                        info. name = cursor.getString(cursor.getColumnIndex("name"));
+                        info. phone = cursor.getString(cursor.getColumnIndex("phone"));
+                        info. sex = cursor.getString(cursor.getColumnIndex("sex"));
+
+                        contactList.add(info);
+
+                        /*Log.d(TAG, "id："+id);
                         Log.d(TAG, "名字："+name);
                         Log.d(TAG, "电话："+phone);
-                        Log.d(TAG, "性别："+sex);
+                        Log.d(TAG, "性别："+sex);*/
                     }
+
                 }
+                QueryAdapter adapter=new QueryAdapter(SecondActivity.this,R.layout.item_sql,contactList);
+                ListView listview=(ListView)findViewById(R.id.view);
+                listview.setAdapter(adapter);
             }
+
         });
     }
 }
